ea59ad337ed66478e1d90c3d4454bc9d14488b5c
Ptk Plum <88824754+PtkPlum@users.noreply.github.com>
2021-09-09 23:17:49 +0700

    Update LinearRegression.py

--- Statistical Model/LinearRegression.py
+++ Statistical Model/LinearRegression.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-# Q4
+# Multiple Linear Regression with 2 variables and 1 target
 data = np.array([[10, 50, 80, 70], 
                  [20, 60, 90, 50], 
                  [30, 80, 100, 90]])
+
 
 X1 = data[0]
 X2 = data[1]
@@ -17,14 +18,20 @@ Y_Sum  = np.sum(Y)
 
 X1sq_Sum = np.sum(X1 ** 2)
 X2sq_Sum = np.sum(X2 ** 2)
-Ysq_Sum  = np.sum(Y  ** 2)
 
 X1X2_Sum = np.sum(X1 * X2)
 X1Y_Sum  = np.sum(X1 * Y )
 X2Y_Sum  = np.sum(X2 * Y)
 
-b1 = (X2sq_Sum * X1Y_Sum - X1X2_Sum * X2Y_Sum) / (X1sq_Sum * X2sq_Sum - X1X2_Sum ** 2)
-b2 = (X1sq_Sum * X2Y_Sum - X1X2_Sum * X1Y_Sum) / (X1sq_Sum * X2sq_Sum - X1X2_Sum ** 2)
+x1sq_Sum = X1sq_Sum - (X1_Sum ** 2 / N)
+x2sq_Sum = X2sq_Sum - (X2_Sum ** 2 / N)
+
+x1x2_Sum = X1X2_Sum - X1_Sum * X2_Sum / N 
+x1y_Sum  = X1Y_Sum  - X1_Sum * Y_Sum / N 
+x2y_Sum  = X2Y_Sum  - X2_Sum * Y_Sum / N
+
+b1 = (x2sq_Sum * x1y_Sum - x1x2_Sum * x2y_Sum) / (x1sq_Sum * x2sq_Sum - x1x2_Sum ** 2)
+b2 = (x1sq_Sum * x2y_Sum - x1x2_Sum * x1y_Sum) / (x1sq_Sum * x2sq_Sum - x1x2_Sum ** 2)
 a  = (Y_Sum - b1 * X1_Sum - b2 * X2_Sum) / N
 
 print("a =", a)
